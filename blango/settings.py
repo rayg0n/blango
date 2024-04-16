f79b3b8b1600de278277118ebc676ad354ce1b81
Anup Adhikari <aanupiw@gmail.com>
2024-04-16 17:29:31 +0000

    database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -58,10 +58,15 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
-        'crispy_bootstrap5'
+        'crispy_bootstrap5',
+        "debug_toolbar",
     ]
+    
+    INTERNAL_IPS = ["192.168.11.179"]
+
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
