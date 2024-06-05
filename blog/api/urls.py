9f74a7d7e9334a302b8171a18ce18b05f499a7fc
Anup Adhikari <aanupiw@gmail.com>
2024-06-05 02:28:02 +0000

    Viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,16 +1,25 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import PostList, PostDetail, UserDetail, TagViewSet, PostViewSet
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.include_format_suffixes = False
+router.register("posts", PostViewSet)
+
+
 
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    # path("posts/", PostList.as_view(), name="api_post_list"),
+    # path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
+    path("", include(router.urls))
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
