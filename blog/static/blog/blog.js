0ac15e91398755c3ae099aaae9e88b709c9eb9c8
Anup Adhikari <aanupiw@gmail.com>
2024-06-07 14:49:24 +0000

    JS

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,4 +1,18 @@
-const framework = 'Django'
-const language = 'Python'
-alert(framework + ' is written in ' + language)
+function sayHello(yourName) {
+  if (yourName === undefined) {
+      console.log('Hello, no name')
+  } else {
+       console.log('Hello, ' + yourName)
+  }
+}
 
+const yourName = 'Your Name'  // Put your name here
+
+console.log('Before setTimeout')
+
+setTimeout(() => {
+    sayHello(yourName)
+  }, 2000
+)
+
+console.log('After setTimeout')
